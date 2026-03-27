c8082f28c4503e920f63f2e129b4f459228c7264
Al Amin Robin <wpdevrobin@Als-MacBook-Air.lan>
2026-03-27 20:36:22 +0600

    Integrate Vercel Speed Insights into React architecture v3.16.0 🛡️🚀🏆

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import { lazy, Suspense } from 'react';
 import { LazyMotion, domMax } from 'framer-motion';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
@@ -87,6 +88,7 @@ function AppContent() {
         </svg>
       </a>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
